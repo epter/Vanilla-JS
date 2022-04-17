31ded1aece21975fdc7c3bf1089a87db4ea742a7
epter <82642114+epter@users.noreply.github.com>
2022-04-17 18:52:10 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,32 @@
+
+function sayHello(nameOfPerson, age), {
+function sayHello(){
+ console.log("hello my name is C");
+}
+
+sayHello("nico");
+
+function sayHello(nameOfPerson, age), {
+function sayHello(){
+ console.log("hello my name is C");
+}
+sayHello("nico",1 0);
+
+Nan 숫자가 아님
+
+function plus (8, b) {
+console.log(a+b);
+}
+
+plus (8,60);
+
+function divide(a,b){
+console.log(a/b);}
+
+divide(98, 20)
+
+ci
+
+
+
+
